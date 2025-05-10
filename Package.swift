cd2c7f19d60f1415cf8f84062d0d7dfb7f42267a
CodeWithTamim <CodeWithTamim@users.noreply.github.com>
2025-05-10 09:08:50 +0000

    XrayKit Release 1.0.1

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "XrayKit",
-      url: "https://github.com/CodeWithTamim/XrayKit/releases/download/1.0.0/XrayKit.xcframework.zip",
-      checksum: "ea55ce8fdda721bcb843233124e7d0a8f9259aba78d5da3421e40e7075956108"
+      url: "https://github.com/CodeWithTamim/XrayKit/releases/download/1.0.1/XrayKit.xcframework.zip",
+      checksum: "ce4f17df515a95230bbfd2c81e8fa047938bce1e95842b5517abf7a53aeab712"
     )
   ]
 )
